76be9957204b1a5e0acb5a3b99897346096a5184
phemms <phemmsbalo@gmail.com>
2021-03-17 00:39:07 +0200

    17/03/21

--- Exercises/Part 1/Exercise 012/Program.cs
+++ Exercises/Part 1/Exercise 012/Program.cs
@@ -6,7 +6,14 @@ namespace exercise_12
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+            Console.WriteLine("Give a number!");
+            string userInput = Console.ReadLine();
+            int value = Convert.ToInt32(userInput);
+            Console.WriteLine("You gave " + value);
+            Console.WriteLine("Give a number!");
+            userInput = Console.ReadLine();
+            value = Convert.ToInt32(userInput);
+            Console.WriteLine("You gave " + value);
 
     }
   }
