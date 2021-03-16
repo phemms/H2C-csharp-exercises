cdf1d0b14b5a6bd8fcba1b9baf6f662ad6403299
phemms <phemmsbalo@gmail.com>
2021-03-16 14:12:45 +0200

    Update Program.cs
    16/03/21

--- Exercises/Part 1/Exercise 010/Program.cs
+++ Exercises/Part 1/Exercise 010/Program.cs
@@ -6,7 +6,17 @@ namespace exercise_10
     {
         public static void Main(string[] args)
         {
-          
+
+            Console.WriteLine("I will tell a story, but I need some information.\n"+
+                "Give a name for the main character:");
+            string mssg = Console.ReadLine();
+            Console.WriteLine("Give the character a profession:");
+            string prof = Console.ReadLine();
+            Console.WriteLine("Here is the story:\n"
+                + "Once upon a time there was a " + prof + " called " + mssg + "\n" +
+                "On her way to work, " + mssg + " often pondered  what being " + prof + " meant to them\n" +
+                 "When you work as a " + prof + " you meet interesting people.\n" + mssg +
+                 " enjoys their work as " + prof + ", The end.");
 
 
         }
