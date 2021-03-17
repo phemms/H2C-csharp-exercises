2595bf519ea66b593a12af3b6f28eacc2b48566f
phemms <phemmsbalo@gmail.com>
2021-03-17 10:12:53 +0200

    17/3/21

--- Exercises/Part 1/Exercise 015/Program.cs
+++ Exercises/Part 1/Exercise 015/Program.cs
@@ -7,8 +7,26 @@ namespace exercise_15
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            Console.WriteLine("Give a string:");
+            string mssg = Console.ReadLine();
 
-    }
+            Console.WriteLine("Give an integer:");
+            string numInput = Console.ReadLine();
+            int num = Convert.ToInt32(numInput);
+            
+            Console.WriteLine("Give a double:");
+            string doubInput = Console.ReadLine();
+            double doubVal = Convert.ToDouble(doubInput);
+            
+            Console.WriteLine("Give a boolean");
+            string boolInput = Console.ReadLine();
+            bool boolVal = Convert.ToBoolean(boolInput);
+            
+            Console.WriteLine("Your string: " + mssg);
+            Console.WriteLine("Your integer: " + num);
+            Console.WriteLine("Your double: " + doubVal);
+            Console.WriteLine("Your boolean: " + boolVal);
+
+        }
   }
 }
