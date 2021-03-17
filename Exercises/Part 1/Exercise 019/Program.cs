be868d04d0fba7a0804d0acd15024d3a0334465e
phemms <phemmsbalo@gmail.com>
2021-03-17 22:49:20 +0200

    17/03/21

--- Exercises/Part 1/Exercise 019/Program.cs
+++ Exercises/Part 1/Exercise 019/Program.cs
@@ -7,8 +7,15 @@ namespace exercise_19
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            Console.WriteLine("Give the first number!");
+            string num = Console.ReadLine();
+            int firstNum = Convert.ToInt32(num);
+            Console.WriteLine("Give the second number!");
+            num = Console.ReadLine();
+            int secondNum = Convert.ToInt32(num);
+            int sum = firstNum + secondNum;
+            Console.WriteLine(firstNum + " + " + secondNum + " = " + sum);
 
-    }
+        }
   }
 }
