17b7b170e68d989ee88759e8d9920a6d53d42799
phemms <phemmsbalo@gmail.com>
2021-03-17 23:23:42 +0200

    17/3/21

--- Exercises/Part 1/Exercise 023/Program.cs
+++ Exercises/Part 1/Exercise 023/Program.cs
@@ -7,8 +7,17 @@ namespace exercise_23
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            Console.WriteLine("Give the first number!");
+            string num = Console.ReadLine();
+            int firstNum = Convert.ToInt32(num);
+            Console.WriteLine("Give the second number!");
+            num = Console.ReadLine();
+            int secondNum = Convert.ToInt32(num);
+            Console.WriteLine(firstNum + " + " + secondNum + " = " + (firstNum+secondNum));
+            Console.WriteLine(firstNum + " - " + secondNum + " = " + (firstNum-secondNum));
+            Console.WriteLine(firstNum + " * " + secondNum + " = " + firstNum * secondNum);
+            Console.WriteLine(firstNum + " + " + secondNum + " = " + (double)firstNum / secondNum);
 
-    }
+        }
   }
 }
