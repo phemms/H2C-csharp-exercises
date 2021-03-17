9d1e3b895fba854d7b0b268ad676b6dbd90a6482
phemms <phemmsbalo@gmail.com>
2021-03-17 09:55:32 +0200

    17/03/21

--- Exercises/Part 1/Exercise 014/Program.cs
+++ Exercises/Part 1/Exercise 014/Program.cs
@@ -6,9 +6,18 @@ namespace exercise_14
   {
     public static void Main(string[] args)
     {
-      Console.WriteLine("Give me the truth!");
-      // Write your code here:
+            Console.WriteLine("Give me the truth!");
+            string truth = Console.ReadLine();
+            bool boolVal = Convert.ToBoolean(truth);
+            Console.WriteLine(boolVal);
 
-    }
+            Console.WriteLine("Give me the truth!");
+            truth = Console.ReadLine();
+            boolVal = Convert.ToBoolean(truth);
+            Console.WriteLine(boolVal);
+
+
+
+        }
   }
 }
