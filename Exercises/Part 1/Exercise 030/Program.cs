01b004e72f4870ce7432224b366308e11eaffcb9
phemms <phemmsbalo@gmail.com>
2021-03-23 22:29:44 +0200

    23/03/21

--- Exercises/Part 1/Exercise 030/Program.cs
+++ Exercises/Part 1/Exercise 030/Program.cs
@@ -6,8 +6,43 @@ namespace exercise_30
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+            Console.WriteLine("Give your percent [0 - 100]:");
+            string userInput = Console.ReadLine();
+            int score = Convert.ToInt32(userInput);
+            if (score < 0)
+            {
+                Console.WriteLine("Impossible");
+            }
 
-    }
+            else if (score < 50)
+            {
+                Console.WriteLine("Fail");
+            }
+            else if (score < 60)
+            {
+                Console.WriteLine("Grade: 1");
+            }
+            else if (score < 70)
+            {
+                Console.WriteLine("Grade: 2");
+            }
+            else if (score < 80)
+            {
+                Console.WriteLine("Grade: 3");
+            }
+            else if (score < 90)
+            {
+                Console.WriteLine("Grade: 4");
+            }
+            else if (score < 100)
+            {
+                Console.WriteLine("Grade: 5");
+            }
+            else
+            {
+                Console.WriteLine("Outstanding");
+            }
+
+        }
   }
 }
