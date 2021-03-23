53f7a1b350a1eb4b1c1ddc39de4ccb48209365fe
phemms <phemmsbalo@gmail.com>
2021-03-23 22:58:13 +0200

    23/03/21

--- Exercises/Part 1/Exercise 033/Program.cs
+++ Exercises/Part 1/Exercise 033/Program.cs
@@ -6,7 +6,18 @@ namespace exercise_33
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+            Console.WriteLine("Give the first string: ");
+            string userInput = Console.ReadLine();
+            Console.WriteLine("Give the second string: ");
+            string secUserInput = Console.ReadLine();
+            if(userInput == secUserInput)
+            {
+                Console.WriteLine("Echo!");
+            }
+            else
+            {
+                Console.WriteLine("Nope!");
+            }
 
     }
   }
