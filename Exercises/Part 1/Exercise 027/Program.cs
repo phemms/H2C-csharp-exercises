8da0534023bec4d48af04be8c713a4f93a2db594
phemms <phemmsbalo@gmail.com>
2021-03-23 21:49:57 +0200

    21/03/21

--- Exercises/Part 1/Exercise 027/Program.cs
+++ Exercises/Part 1/Exercise 027/Program.cs
@@ -6,7 +6,19 @@ namespace exercise_27
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+            Console.WriteLine("Give a number:");
+            string userInput = Console.ReadLine();
+            int num = Convert.ToInt32(userInput);
+            if (num > 0)
+            {
+                Console.WriteLine("It is positive");
+            }
+
+            else
+            {
+                Console.WriteLine("It is not positive");
+            }
+                
 
     }
   }
