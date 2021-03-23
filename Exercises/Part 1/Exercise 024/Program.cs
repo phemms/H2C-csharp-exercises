973283dac890c1fe717d9590a11f93204a5cff40
phemms <phemmsbalo@gmail.com>
2021-03-23 21:26:19 +0200

    23/3/21

--- Exercises/Part 1/Exercise 024/Program.cs
+++ Exercises/Part 1/Exercise 024/Program.cs
@@ -7,7 +7,13 @@ namespace exercise_24
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            Console.WriteLine("Your speed:");
+            string userInput = Console.ReadLine();
+            int speed = Convert.ToInt32(userInput);
+            if(speed > 120)
+            {
+                Console.WriteLine("Speeding!");
+            }
       
     }
   }
