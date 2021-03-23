35257b895379cfc83b73ba5271a13008e22c785d
phemms <phemmsbalo@gmail.com>
2021-03-23 22:10:12 +0200

    23/03/21

--- Exercises/Part 1/Exercise 029/Program.cs
+++ Exercises/Part 1/Exercise 029/Program.cs
@@ -6,8 +6,26 @@ namespace exercise_29
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+            Console.WriteLine("Give the first number!");
+            string userInput = Console.ReadLine();
+            int firstNum = Convert.ToInt32(userInput);
+            Console.WriteLine("Give the second number");
+            userInput = Console.ReadLine();
+            int secondNum = Convert.ToInt32(userInput);
+            if (firstNum > secondNum)
+            {
+                Console.WriteLine("It larger number is " + firstNum);
+            }
 
-    }
+            else if(firstNum < secondNum)
+            {
+                Console.WriteLine("The larger number is " + secondNum);
+            }
+            else if (firstNum == secondNum)
+            {
+                Console.WriteLine("They are eqaul!");
+            }
+
+        }
   }
 }
