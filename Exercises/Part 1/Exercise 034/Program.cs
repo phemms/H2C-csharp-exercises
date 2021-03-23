19dd2b27553eef922b2875d0d3a3ab3119ea82eb
phemms <phemmsbalo@gmail.com>
2021-03-24 00:58:39 +0200

    24/03/21

--- Exercises/Part 1/Exercise 034/Program.cs
+++ Exercises/Part 1/Exercise 034/Program.cs
@@ -6,7 +6,15 @@ namespace exercise_34
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      while (true)
+            {
+                Console.WriteLine("Do you want to continue?");
+                string userInput = Console.ReadLine();
+                if (userInput == "no")
+                {
+                    break;
+                }
+            }
 
     }
   }
