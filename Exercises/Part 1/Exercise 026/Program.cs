e4e5fdd34af89c943e892e25d9f4fe7b9a6d1e35
phemms <phemmsbalo@gmail.com>
2021-03-23 21:36:35 +0200

    23/03/21

--- Exercises/Part 1/Exercise 026/Program.cs
+++ Exercises/Part 1/Exercise 026/Program.cs
@@ -2,12 +2,18 @@
 
 namespace exercise_26
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // Write your code here:
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Give your age:");
+            string userInput = Console.ReadLine();
+            int year = Convert.ToInt32(userInput);
+            if (year < 1900)
+            {
+                Console.WriteLine("Your're old");
 
+            }
+        }
     }
-  }
 }
