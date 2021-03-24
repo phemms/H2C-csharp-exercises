cffd21a2db80c5d3dca429d03216742b217928e2
phemms <phemmsbalo@gmail.com>
2021-03-24 10:43:14 +0200

    24/03/21

--- Exercises/Part 1/Exercise 040/Program.cs
+++ Exercises/Part 1/Exercise 040/Program.cs
@@ -7,7 +7,26 @@ namespace exercise_40
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            int sum = 0;
+            int count = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Give a number:");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                if (num == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    sum +=num;
+                    count += 1;
+                }
+            }
+            Console.WriteLine("Total sum of numbers: " + sum + "\nTotal amount of numbers: " + count );
+
+        }
     }
-  }
 }
