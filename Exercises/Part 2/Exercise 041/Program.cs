1fcc56dc3381fc4790bb6230c6c9281e2253cec3
phemms <phemmsbalo@gmail.com>
2021-03-24 10:59:29 +0200

    24/03/21

--- Exercises/Part 2/Exercise 041/Program.cs
+++ Exercises/Part 2/Exercise 041/Program.cs
@@ -7,8 +7,9 @@ namespace exercise_41
     public static void Main(string[] args)
     {
 
-      // Write your code here:
-      
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num * num);
+            
     }
   }
 }
