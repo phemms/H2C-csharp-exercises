ccd9523de5600d6fb6b64653a6860829a3382b71
phemms <phemmsbalo@gmail.com>
2021-03-24 10:30:53 +0200

    24/03/21

--- Exercises/Part 1/Exercise 038/Program.cs
+++ Exercises/Part 1/Exercise 038/Program.cs
@@ -6,8 +6,29 @@ namespace exercise_38
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+            int count = 0;
 
+            while (true)
+            {
+                Console.WriteLine("Give a number:");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                if (num == 0)
+                {
+                    break;
+                }
+                else if (num > 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    count +=1;
+                }
+            }
+            Console.WriteLine("Total amount of negative numbers: " + count);
+
+
+        }
     }
-  }
 }
