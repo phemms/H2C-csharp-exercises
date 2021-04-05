746f5e6fbfba7db63136e163c77054fa7e7018bd
phemms <phemmsbalo@gmail.com>
2021-04-05 20:29:39 +0300

    5/4/21

--- Exercises/Part 2/Exercise 053/Program.cs
+++ Exercises/Part 2/Exercise 053/Program.cs
@@ -6,12 +6,15 @@ namespace exercise_53
   {
     public static void Main(String[] args)
     {
-      // Call your method here:
+            Division(7, 3);
 
 
     }
 
-    // Write your method here:
+        public static void Division(int numerator, int denominator)
+        {
+            Console.WriteLine((double)numerator / denominator);
+        }
 
   }
 }
