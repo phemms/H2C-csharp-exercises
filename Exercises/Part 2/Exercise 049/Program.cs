ecbd7025c41dcde1fbc5e7d91c871b57bd8e19e8
phemms <phemmsbalo@gmail.com>
2021-04-05 20:08:37 +0300

    5/4/21

--- Exercises/Part 2/Exercise 049/Program.cs
+++ Exercises/Part 2/Exercise 049/Program.cs
@@ -6,14 +6,11 @@ namespace exercise_49
   {
     public static void Main(string[] args)
     {
-            Console.WriteLine("How many times?");
-            int num = Convert.ToInt32(Console.ReadLine());
-            int i = 0;
-            while (i < num)
-            {
+            
+            
                 PrintPhrase();
-                i++;
-            }
+               
+            
             
  
     }
