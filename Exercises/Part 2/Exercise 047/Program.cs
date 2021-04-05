450af8223029868b46b07e39c3f253b2bc7f81f1
phemms <phemmsbalo@gmail.com>
2021-04-05 16:13:53 +0300

    5/4/21

--- Exercises/Part 2/Exercise 047/Program.cs
+++ Exercises/Part 2/Exercise 047/Program.cs
@@ -6,9 +6,16 @@ namespace exercise_47
   {
     public static void Main(string[] args)
     {
+            Console.WriteLine("Where to?");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Where from?");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            for (int i = num1; i <= num; i++)
+            {
+                Console.WriteLine(i);
+            }
 
-      // Write your code here:
-      
-    }
+        }
   }
 }
