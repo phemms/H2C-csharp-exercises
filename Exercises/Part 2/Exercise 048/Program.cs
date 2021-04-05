d3fea7cc32a21c92f2b4d01aff3c6ed28360f6c3
phemms <phemmsbalo@gmail.com>
2021-04-05 17:29:05 +0300

    5/4/21

--- Exercises/Part 2/Exercise 048/Program.cs
+++ Exercises/Part 2/Exercise 048/Program.cs
@@ -6,9 +6,26 @@ namespace exercise_48
   {
     public static void Main(string[] args)
     {
+            int sum = 0; int count = 0;int even = 0;int odd = 0;
+            Console.WriteLine("Give numbers: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            while(num != -1)
+            {
+                count += 1;
+                sum += num;
+                if (num % 2 == 0) even++;
+                else odd++;
+                num = Convert.ToInt32(Console.ReadLine());
+                
+            }
+            double average = (double)sum / count;
+            Console.WriteLine("Thx! Bye!");
+            Console.WriteLine("Sum: " + sum);
+            Console.WriteLine("Numbers: " + count);
+            Console.WriteLine("Average: " + average);
+            Console.WriteLine("Even: " + even);
+            Console.WriteLine("Odd: " + odd);
 
-      // Write your code here:
- 
-    }
+        }
   }
 }
