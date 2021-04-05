def511e45db11dda0f5721609cf3c560c6b835fa
phemms <phemmsbalo@gmail.com>
2021-04-05 20:34:12 +0300

    5/4/21

--- Exercises/Part 2/Exercise 054/Program.cs
+++ Exercises/Part 2/Exercise 054/Program.cs
@@ -6,11 +6,17 @@ namespace exercise_54
   {
     public static void Main(String[] args)
     {
-      // Call your method here:
+            DivisibleByThreeInRange(2, 10);
 
-    }
+        }
 
-    // Write your method here:
+        public static void DivisibleByThreeInRange(int beginning, int end)
+        {
+            for(int i = beginning; i <= end; i++)
+            {
+                if (i % 3 == 0) Console.WriteLine(i);
+            }
+        }
 
   }
 }
