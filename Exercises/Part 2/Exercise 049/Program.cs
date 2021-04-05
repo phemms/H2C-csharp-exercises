a0877d05f889f5e5392923730ddb4d651a54737b
phemms <phemmsbalo@gmail.com>
2021-04-05 19:59:50 +0300

    5/4/21

--- Exercises/Part 2/Exercise 049/Program.cs
+++ Exercises/Part 2/Exercise 049/Program.cs
@@ -6,12 +6,21 @@ namespace exercise_49
   {
     public static void Main(string[] args)
     {
-      // Call your method here:
+            Console.WriteLine("How many times?");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            while (i < num)
+            {
+                PrintPhrase();
+                i++;
+            }
+            
  
     }
 
-    // Write your method here:
+    
     public static void PrintPhrase() {
+            Console.WriteLine("In a hole in the ground there lived a method");
 
     }
   }
