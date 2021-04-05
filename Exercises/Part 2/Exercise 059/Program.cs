159d0950923a952dafe06408cce13822d1363f56
phemms <phemmsbalo@gmail.com>
2021-04-05 22:18:52 +0300

    5/4/21

--- Exercises/Part 2/Exercise 059/Program.cs
+++ Exercises/Part 2/Exercise 059/Program.cs
@@ -6,11 +6,18 @@ namespace exercise_59
   {
     public static void Main(String[] args)
     {
-      int answer = Greatest(2, 7, 3);
+      int answer = Greatest(2, 7, 10);
       Console.WriteLine("Greatest: " + answer);
     }
 
-    // Write your method here:
+    public static int Greatest(int num1, int num2, int num3)
+        {
+            if (num1 > num2)
+                if (num1 > num3) return num1;
+                else return num3;
+            else if (num2 > num3) return num2;
+            else return num3;
+        }
 
   }
 }
