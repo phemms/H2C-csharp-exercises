25038ce48a756b70456ba84564a83036c6475399
phemms <phemmsbalo@gmail.com>
2021-04-16 17:06:40 +0300

    16/4/21

--- Exercises/Part 3/Exercise 072/Program.cs
+++ Exercises/Part 3/Exercise 072/Program.cs
@@ -7,7 +7,35 @@ namespace exercise_72
   {
     public static void Main(string[] args)
     {
+            List<int> list = new List<int>();
+            while (true)
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                if (input == 9999)
+                {
+                    break;
+                }
+                list.Add(input);
+            }
+            //int c = 0;
+            int s = list[0];
+            for (int i = 0; i < list.Count; i++)
+            {
+                int num = list[i];
+                if (s > num) s = num;
+                {
 
+                }
+            }
+            Console.WriteLine("smallest number: " + s);
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] == s)
+                {
+                    Console.WriteLine("Found at index " + i);
+                }
+            }
+
+        }
     }
-  }
 }
