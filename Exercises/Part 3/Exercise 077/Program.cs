a61b3077e7bad0dcb37ffbf9657e9cdac3f5679d
phemms <phemmsbalo@gmail.com>
2021-04-18 12:11:46 +0300

    18/04/21

--- Exercises/Part 3/Exercise 077/Program.cs
+++ Exercises/Part 3/Exercise 077/Program.cs
@@ -7,8 +7,22 @@ namespace exercise_77
   {
     public static void Main(string[] args)
     {
-      // You can try your method here if you want
-    }
+     List<string> strings = new List<string>();
+     strings.Add("First");
+     strings.Add("Second");
+     strings.Add("Third");
+
+     strings.ForEach(Console.WriteLine);
+
+     RemoveLast(strings);
+     //RemoveLast(strings);
+
+     strings.ForEach(Console.WriteLine);
+        }
+    public static void RemoveLast(List<string> strings)
+        {
+            strings.RemoveAt(strings.Count - 1);
+        }
 
   }
 }
