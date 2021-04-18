2397df4f11884b8d188c793d14c98b2e60a1c361
phemms <phemmsbalo@gmail.com>
2021-04-18 11:58:37 +0300

    18/04/21

--- Exercises/Part 3/Exercise 076/Program.cs
+++ Exercises/Part 3/Exercise 076/Program.cs
@@ -7,8 +7,26 @@ namespace exercise_76
   {
     public static void Main(string[] args)
     {
+        List<int> numbers = new List<int>();
+        numbers.Add(3);
+        numbers.Add(2);
+        numbers.Add(6);
+        numbers.Add(-1);
+        Console.WriteLine(Sum(numbers));
 
-    }
+        numbers.Add(5);
+        numbers.Add(1);
+        Console.WriteLine(Sum(numbers));
+        }
+    public static int Sum(List<int> numbers)
+        {
+            int sum = 0;
+            foreach(int i in numbers)
+            {
+                sum += i;
+            }
+            return sum;
+        }
 
   }
 }
