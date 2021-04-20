104f8fa2b6be29c862f576b93d00c3b4d54a464e
phemms <phemmsbalo@gmail.com>
2021-04-20 12:00:26 +0300

    20/04/21

--- Exercises/Part 3/Exercise 085/Program.cs
+++ Exercises/Part 3/Exercise 085/Program.cs
@@ -7,6 +7,22 @@ namespace exercise_85
   {
     public static void Main(string[] args)
     {
+            string userName1 = "alex";
+            string password1 = "sunshine";
+            string userName2 = "emma";
+            string password2 = "haskell";
+            Console.WriteLine("Enter username:");
+            string um = Console.ReadLine();
+            Console.WriteLine("Enter password:");
+            string pw = Console.ReadLine();
+            if (um == userName1 && pw == password1)
+               Console.WriteLine("You have successfully logged in");
+            else if (um == userName2 && pw == password2)
+               Console.WriteLine("You have successfully logged in!");
+            else Console.WriteLine("Incorrect username or password!");
+
+
+
 
 
     }
