2f809582540754ea68f9f049eadd8fb1e8539d06
phemms <phemmsbalo@gmail.com>
2021-04-20 15:32:40 +0300

    20/04/21

--- Exercises/Part 3/Exercise 092/Program.cs
+++ Exercises/Part 3/Exercise 092/Program.cs
@@ -7,8 +7,30 @@ namespace exercise_92
   {
     public static void Main(string[] args)
     {
+            int small = 2020;
+            int size = 0;
+            string name = "w";
 
-    }
+            while (true)
+            {
+                string phrase = Console.ReadLine();
+                if (phrase == "") break;
+                string[] words = phrase.Split(',');
+                int year = Convert.ToInt32(words[1]);
+                int length = words[0].Length;
+                if (year < small)
+                {
+                    small = year;
+                }
+                if(length > size)
+                {
+                    size = length;
+                    name = words[0];
+                }
+            }
+            Console.WriteLine("Longest name: " + name);
+            Console.WriteLine("Highest age: " + (2020 - small));
+        }
   }
 }
 
