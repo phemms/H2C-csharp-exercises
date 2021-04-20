a9236eb2afc47b5dac59d6c146adaeae606a8a08
phemms <phemmsbalo@gmail.com>
2021-04-20 12:36:47 +0300

    20/4/21

--- Exercises/Part 3/Exercise 087/Program.cs
+++ Exercises/Part 3/Exercise 087/Program.cs
@@ -7,8 +7,18 @@ namespace exercise_87
   {
     public static void Main(string[] args)
     {
-
-    }
+            while (true)
+            {
+                string phrase = Console.ReadLine();
+                if (phrase == "") break;
+                string[] words = phrase.Split(' ');
+                foreach (string t in words)
+                {
+                    if(t.Contains("av"))
+                    Console.WriteLine(t);
+                }
+            }
+        }
 
   }
 
