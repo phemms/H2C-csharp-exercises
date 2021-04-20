f53099c896a585836d3180fb26e0384e289196c7
phemms <phemmsbalo@gmail.com>
2021-04-20 12:57:52 +0300

    20/04/21

--- Exercises/Part 3/Exercise 090/Program.cs
+++ Exercises/Part 3/Exercise 090/Program.cs
@@ -7,9 +7,18 @@ namespace exercise_90
   {
     public static void Main(string[] args)
     {
+            int big = 0;
+            while (true)
+            {
+                string phrase = Console.ReadLine();
+                if (phrase == "") break;
+                string[] words = phrase.Split(',');
+                int age = Convert.ToInt32(words[1]);
+                if (age > big) big = age;
+            }
+            Console.WriteLine("Age of oldest: " + big);
 
-
-    }
+        }
   }
 }
 
