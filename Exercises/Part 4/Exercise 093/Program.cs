b962eb1e3fcc4e5709578ab2d8d952c3dc6b277f
phemms <phemmsbalo@gmail.com>
2021-04-21 13:04:40 +0300

    21/04/21

--- Exercises/Part 4/Exercise 093/Program.cs
+++ Exercises/Part 4/Exercise 093/Program.cs
@@ -6,10 +6,12 @@ namespace exercise_93
   {
     public static void Main(string[] args)
     {
-      // DO NOT TOUCH THE OTHER FILE!
-      // Do your code here!
-
-    }
+            // DO NOT TOUCH THE OTHER FILE!
+            // Do your code here!
+            Account TerosAccount = new Account("TerosAccount", 100.0);
+            TerosAccount.Deposit(20);
+            Console.WriteLine(TerosAccount);
+        }
   }
 }
 
