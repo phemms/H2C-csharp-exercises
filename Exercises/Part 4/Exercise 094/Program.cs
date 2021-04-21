b872d9545ebeea7066398355eed4e02fba1367c7
phemms <phemmsbalo@gmail.com>
2021-04-21 22:44:18 +0300

    21/04/21

--- Exercises/Part 4/Exercise 094/Program.cs
+++ Exercises/Part 4/Exercise 094/Program.cs
@@ -6,8 +6,14 @@ namespace exercise_94
   {
     public static void Main(string[] args)
     {
-      // DO NOT TOUCH THE OTHER FILE!
-      // Do your code here!
+            // DO NOT TOUCH THE OTHER FILE!
+            // Do your code here!
+            Account account = new Account("Heikki's account", 1000);
+            Account account1 = new Account("Personal account", 0);
+            account.Withdrawal(100.0);
+            account1.Deposit(100.0);
+            Console.WriteLine(account);
+            Console.WriteLine(account1);
 
     }
   }
