d9ec9d0ab0685142676e4bd500d533217f79230a
phemms <phemmsbalo@gmail.com>
2021-04-22 12:46:55 +0300

    22/4/21

--- Exercises/Part 4/Exercise 100/Program.cs
+++ Exercises/Part 4/Exercise 100/Program.cs
@@ -2,12 +2,45 @@
 
 namespace exercise_100
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
+        public static void Main(string[] args)
+        {
+            Debt mortgage = new Debt(120000.0, 1.01);
+            mortgage.PrintBalance();
 
-    }
+            mortgage.WaitOneYear();
+            mortgage.PrintBalance();
+
+            // Wait 20 years
+            int years = 0;
+            while (years < 20)
+            {
+                mortgage.WaitOneYear();
+                years = years + 1;
+            }
+
+            mortgage.PrintBalance();
+        }
+        public class Debt
+            {
+                private double balance;
+                private double interestRate;
+                public Debt(double initialBalance, double initialInterestRate)
+            {
+                this.balance = initialBalance;
+                this.interestRate = initialInterestRate;
+            }
+            public void PrintBalance()
+            {
+                Console.WriteLine(this.balance);
+            }
+            public void WaitOneYear()
+            {
+                this.balance = this.balance * this.interestRate;
+            }
+            }
+
   }
 }
 
