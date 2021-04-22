0cdcd56f0e53204b7653250d7bbad4d3e25ab059
phemms <phemmsbalo@gmail.com>
2021-04-22 13:02:59 +0300

    22/4/21

---
+++ Exercises/Part 4/Exercise 101/Dalmatian.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace exercise_101
+{
+    public class Dalmatian
+    {
+        public string name { get; set; }
+        public int spots { get; set; }
+        public Dalmatian(string name, int spots)
+        {
+            this.name = name;
+            this.spots = spots;
+        }
+    }
+}
