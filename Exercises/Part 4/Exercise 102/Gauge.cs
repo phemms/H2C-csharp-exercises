eba2d4667da554cd79ede9c928d6c6323ee437d9
phemms <phemmsbalo@gmail.com>
2021-04-22 13:20:26 +0300

    22/4/21

---
+++ Exercises/Part 4/Exercise 102/Gauge.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace exercise_102
+{
+    public class Gauge
+    {
+        public int value { get; set; }
+        public Gauge()
+        {
+            this.value = 0;
+        }
+        public void Increase()
+        {
+            this.value += 1;
+            if (this.value > 5) this.value = 5;
+        }
+        public void Decrease()
+        {
+            this.value -= 1;
+            if (this.value < 0) this.value = 0;
+        }
+        public bool Full()
+        {
+            if (this.value == 5) return true;
+            else return false;
+        }
+    }
+}
