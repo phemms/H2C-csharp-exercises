e60a079d5849cd421e78740e356a81c6c2c8fcf6
phemms <phemmsbalo@gmail.com>
2021-04-22 11:45:14 +0300

    22/4/21

--- Exercises/Part 4/Exercise 098/Program.cs
+++ Exercises/Part 4/Exercise 098/Program.cs
@@ -6,11 +6,29 @@ namespace exercise_98
   {
     public static void Main(string[] args)
     {
-      // Create Product.cs and follow the instructions.
-      // You can test your class with this.
+            // Create Product.cs and follow the instructions.
+            // You can test your class with this.
+            Product product = new Product("Banana", 1.1, 13);
+            product.PrintProduct();
       
 
     }
+        public class Product
+        {
+            private double price;
+            private int quantity;
+            private string name;
+            public Product(string name, double price, int quantity)
+            {
+                this.price = price;
+                this.quantity = quantity;
+                this.name = name;
+            }
+            public void PrintProduct()
+            {
+                Console.WriteLine(this.name +": price " + this.price + ": " + this.quantity + " pcs");
+            }
+        }
   }
 }
 
