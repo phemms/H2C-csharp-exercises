2d24d92fff7f726df557f815c2f98f5883a42ed5
phemms <phemmsbalo@gmail.com>
2021-04-28 01:21:30 +0300

    28/4/21

--- Exercises/Part 4/Exercise 111/Program.cs
+++ Exercises/Part 4/Exercise 111/Program.cs
@@ -9,6 +9,29 @@ namespace exercise_111
     {
       // IMPLEMENT YOUR CODE IN HERE!
       List<PersonalInformation> list = new List<PersonalInformation>();
+            while (true)
+            {
+                Console.WriteLine("First name: ");
+                string fName = Console.ReadLine();
+                if (fName == "")
+                {
+                    break;
+                }
+                Console.WriteLine("Last name: ");
+                string lName = Console.ReadLine();
+                Console.WriteLine("Identification number: ");
+                string id = Console.ReadLine();
+
+                
+                list.Add(new PersonalInformation(fName, lName, id));
+            }
+            Console.WriteLine();
+            foreach (PersonalInformation p in list)
+            {
+                Console.WriteLine(p.firstName + " " + p.lastName);
+            }
+
+            
 
     }
   }
