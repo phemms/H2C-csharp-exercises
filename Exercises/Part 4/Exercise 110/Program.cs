9f0fd4e99a40bc5ee680d88e8e2039467b0a5128
phemms <phemmsbalo@gmail.com>
2021-04-28 00:34:25 +0300

    28/04/21

--- Exercises/Part 4/Exercise 110/Program.cs
+++ Exercises/Part 4/Exercise 110/Program.cs
@@ -7,7 +7,20 @@ namespace exercise_110
   {
     public static void Main(string[] args)
     {
-      // IMPLEMENT YOUR CODE IN HERE!
+            // IMPLEMENT YOUR CODE IN HERE!
+            List<Item> item = new List<Item>();
+            while (true)
+            {
+                Console.WriteLine("Name: ");
+                string name = Console.ReadLine();
+                if (name == "") break;
+                item.Add(new Item(name));
+            }
+            foreach(Item i in item)
+            {
+                Console.WriteLine(i);
+            }
+
     }
   }
 }
