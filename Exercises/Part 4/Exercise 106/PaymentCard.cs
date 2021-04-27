d3fb81c16759069dd1d468548ffc0152dae2f4c8
phemms <phemmsbalo@gmail.com>
2021-04-27 13:40:48 +0300

    27/04/21

---
+++ Exercises/Part 4/Exercise 106/PaymentCard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace exercise_106
+{
+    public class PaymentCard
+    {
+        public double balance { get; set; }
+        public PaymentCard(double openingBalance)
+        {
+            this.balance = openingBalance;
+        }
+        public override string ToString()
+        {
+            return "This card has a balance of " + this.balance + " euros.";
+        }
+    }
+}
