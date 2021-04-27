d80ac0dc345a96b37b84e749b23d3c8e291a5d3f
phemms <phemmsbalo@gmail.com>
2021-04-28 01:35:27 +0300

    28/4/21

--- Exercises/Part 4/Exercise 112/Program.cs
+++ Exercises/Part 4/Exercise 112/Program.cs
@@ -7,10 +7,32 @@ namespace exercise_112
   {
     public static void Main(string[] args)
     {
-      // IMPLEMENT YOUR CODE IN HERE!
+            // IMPLEMENT YOUR CODE IN HERE!
+            List<TelevisionProgram> list = new List<TelevisionProgram>();
+            while (true)
+            {
+                Console.WriteLine("Name: ");
+                string name = Console.ReadLine();
+                if (name == "")
+                {
+                    break;
+                }
+                Console.WriteLine("Duration: ");
+                int dur = Convert.ToInt32(Console.ReadLine());
 
 
-    }
+                list.Add(new TelevisionProgram(name, dur));
+            }
+            Console.WriteLine();
+            Console.WriteLine("Program's maximum duration? ");
+            int d = Convert.ToInt32(Console.ReadLine());
+            foreach (TelevisionProgram t in list)
+            {
+                if (t.duration <= d) Console.WriteLine(t);
+            }
+
+
+        }
   }
 }
 
