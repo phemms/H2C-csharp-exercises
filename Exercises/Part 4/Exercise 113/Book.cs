4bc7ea64825dc62a4834d4775d4672b6f9e0117b
phemms <phemmsbalo@gmail.com>
2021-04-28 08:21:27 +0300

    28/4/21

---
+++ Exercises/Part 4/Exercise 113/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace exercise_113
+{
+    public class Book
+    {
+        public string name { get; set; }
+        public int page { get; set; }
+        public int year { get; set; }
+
+        public Book(string bkname, int pgnum, int yr)
+        {
+            this.name = bkname;
+            this.page = pgnum;
+            this.year = yr;
+        }
+        public override string ToString()
+        {
+            return this.name + ", " + this.page + " pages, " + this.year;
+        }
+    }
+}
--- Exercises/Part 4/Exercise 113/Program.cs
+++ Exercises/Part 4/Exercise 113/Program.cs
@@ -7,10 +7,39 @@ namespace exercise_113
   {
     public static void Main(string[] args)
     {
-      // IMPLEMENT YOUR CODE IN HERE!
-      // Remember also to create the file Book.cs and class in it!
+            // IMPLEMENT YOUR CODE IN HERE!
+            // Remember also to create the file Book.cs and class in it!
+            List<Book> books = new List<Book>();
 
-    }
+            while (true)
+            {
+                Console.WriteLine("Name: ");
+                string name = Console.ReadLine();
+                if(name == "")
+                {
+                    break;
+                }
+                Console.WriteLine("Pages: ");
+                int page = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Publication year: ");
+                int year = Convert.ToInt32(Console.ReadLine());
+
+                books.Add(new Book(name, page, year));
+            }
+            Console.WriteLine();
+            Console.WriteLine("What information will be printed? ");
+            string s = Console.ReadLine();
+            foreach(Book b in books)
+            {
+                if (s == "everything")
+                    Console.WriteLine(b);
+                else if (s == "Title")
+                    Console.WriteLine(b.name);
+                else
+                    break;
+            }
+
+        }
   }
 }
 
