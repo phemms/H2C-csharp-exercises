6bae104b62da9adc8e8df0c9eb63cc35551a57ad
phemms <phemmsbalo@gmail.com>
2021-04-28 22:59:32 +0300

    28/4/21

---
+++ Exercises/Part 5/Exercise 119/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace exercise_119
+{
+    public class Timer
+    {
+        private ClockHand hundreds;
+        private ClockHand seconds;
+        public Timer()
+        {
+            this.hundreds = new ClockHand(100);
+            this.seconds = new ClockHand(60);
+            
+        }
+        public void Advance()
+        {
+            this.hundreds.Advance();
+            if (this.hundreds.value == 0)
+                this.seconds.Advance();
+        }
+        public override string ToString()
+        {
+            return seconds + ":" + hundreds;
+        }
+    }
+}
