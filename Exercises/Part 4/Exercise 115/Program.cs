e931aa820485b6ef1f8c9000fd0fe44640eb80fe
phemms <phemmsbalo@gmail.com>
2021-04-28 08:47:39 +0300

    28/4/21

--- Exercises/Part 4/Exercise 115/Program.cs
+++ Exercises/Part 4/Exercise 115/Program.cs
@@ -6,7 +6,15 @@ namespace exercise_115
   {
     public static void Main(string[] args)
     {
+            while (true)
+            {
+                string text = Console.ReadLine();
+                if (text == "end")
+                    break;
 
+                int num = Convert.ToInt32(text);
+                Console.WriteLine(num * num * num);
+            }
     }
   }
 }
