847d6eee1a129f0c9288af42e4f4ce05757051ff
phemms <phemmsbalo@gmail.com>
2021-04-29 08:29:02 +0300

    29/4/21

---
+++ Exercises/Part 5/Exercise 120/Cube.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace exercise_120
+{
+    public class Cube
+    {
+        private int length;
+        public Cube(int edgeLength)
+        {
+            this.length = edgeLength;
+        }
+        public int Volume()
+        {
+            return this.length * this.length * this.length;
+        }
+        public override string ToString()
+        {
+            return "The length of the edge is " + this.length + " the Volume is " + this.Volume();
+        }
+    }
+}
--- Exercises/Part 5/Exercise 120/Program.cs
+++ Exercises/Part 5/Exercise 120/Program.cs
@@ -6,7 +6,10 @@ namespace exercise_120
   {
     public static void Main(string[] args)
     {
-      // You can test your cube here.
+            // You can test your cube here.
+            Cube cube = new Cube(11);
+            cube.Volume();
+            Console.WriteLine(cube);
       
     }
   }
