9560f29abaf442a5ddc3b2b9cad28a7115c369d6
phemms <phemmsbalo@gmail.com>
2021-04-29 08:47:28 +0300

    29/04/21

---
+++ Exercises/Part 5/Exercise 121/Fitbyte.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace exercise_121
+{
+    public class Fitbyte
+    {
+        private int age;
+        private int restingHeartRate;
+        public Fitbyte(int age, int restingHeartRate)
+        {
+            this.age = age;
+            this.restingHeartRate = restingHeartRate;
+        }
+        public double TargetHeartRate(double percentageOfMaximum)
+        {
+            return (206.3 - (0.711 * this.age) - this.restingHeartRate)
+                * percentageOfMaximum + this.restingHeartRate;
+        }
+    }
+}
