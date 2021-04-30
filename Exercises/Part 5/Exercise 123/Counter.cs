df7234ae32e9c4016520df690ddf6848074983a2
phemms <phemmsbalo@gmail.com>
2021-04-30 09:24:16 +0300

    30/4/21

---
+++ Exercises/Part 5/Exercise 123/Counter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace exercise_123
+{
+    public class Counter
+    {
+        public int value { get; set; }
+        public Counter() :this(0)
+        {
+        }
+        public Counter(int startValue)
+        {
+            this.value = startValue;
+        }
+        public void Increase()
+        {
+            this.Increase(1);
+        }
+        public void Decrease()
+        {
+            this.Decrease(1);
+        }
+        public void Increase(int increaseBy)
+        {
+            if(increaseBy > 0)
+            {
+                this.value += increaseBy;
+            }
+        }
+        public void Decrease(int decreaseBy)
+        {
+            if (decreaseBy > 0)
+            {
+                this.value -= decreaseBy;
+            }
+        }
+        public override string ToString()
+        {
+            return "Value is " + this.value;
+            
+        }
+    }
+}
--- Exercises/Part 5/Exercise 123/Program.cs
+++ Exercises/Part 5/Exercise 123/Program.cs
@@ -7,8 +7,12 @@ namespace exercise_123
     public static void Main(string[] args)
     {
       // You can test your code here.
-      Counter counter = new Counter();
-      
-    }
+      Counter counter = new Counter(45);
+            counter.Increase(-1);
+            Console.WriteLine(counter);
+            counter.Decrease();
+            Console.WriteLine(counter);
+
+        }
   }
 }
